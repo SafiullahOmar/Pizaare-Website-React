34ce2158d709d59e5ac06a1adb5853ecc2d23569
Safiullah Omar <omar.safiullah@gmail.com>
2023-06-22 22:19:52 +0430

    es7

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+import React from 'react';
+import BannerImage from '../Assets/pizza.jpeg';
 
 function Home() {
   return (
-    <div>
-      hello world hiiiiii
+    <div className="home">
+      <div className="headerContainer" style={{backgroundImage:`url(${BannerImage}) `}}>
+        <h1>Pidros pizzaareee</h1>
+        <p>Pizaa fit to any taste</p>
+        <Link to="/menu">
+        <button>Order NOW</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
